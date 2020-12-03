11a1591b9b90aa21830074f87d40b8a01749ca81
ChUrl <tobi@urpost.de>
2020-12-03 20:05:51 +0100

    grammaranalyzer template

---
+++ src/main/java/parser/LL1GrammarAnalyzer.java
@@ -0,0 +1,23 @@
+package parser;
+
+import java.util.Map;
+import java.util.Set;
+
+public class LL1GrammarAnalyzer {
+
+    public Set<String> getNullable() {
+        return null;
+    }
+
+    public Map<String, Set<String>> getFirst() {
+        return null;
+    }
+
+    public Map<String, Set<String>> getFollow() {
+        return null;
+    }
+
+    public ILL1ParsingTable getTable() {
+        return null;
+    }
+}
