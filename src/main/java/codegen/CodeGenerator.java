c25c6e0d45983a149262e5e12fb4b4744b944763
ChUrl <tobi@urpost.de>
2021-01-29 21:53:33 +0100

    renaming and FlowGraph Update

--- src/main/java/codegen/CodeGenerator.java
+++ src/main/java/codegen/CodeGenerator.java
@@ -1,7 +1,7 @@
 package codegen;
 
-import codegen.sourcegraph.SourceGraph;
-import codegen.sourcegraph.SourceGraphGenerator;
+import codegen.flowgraph.FlowGraph;
+import codegen.flowgraph.FlowGraphGenerator;
 import parser.ast.AST;
 import parser.ast.ASTNode;
 
@@ -12,8 +12,8 @@ public final class CodeGenerator {
     private CodeGenerator() {}
 
     public static String generateCode(AST tree, Map<ASTNode, String> nodeTypeMap, String source) {
-        final SourceGraphGenerator gen = SourceGraphGenerator.fromAST(tree, nodeTypeMap, source);
-        final SourceGraph graph = gen.generateGraph();
+        final FlowGraphGenerator gen = FlowGraphGenerator.fromAST(tree, nodeTypeMap, source);
+        final FlowGraph graph = gen.generateGraph();
 
         return graph.toString();
     }
